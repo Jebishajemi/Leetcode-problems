fb68cad7e81bc31b1ed5a3ad3d9b7e0a39b9a845
Jebisha <129985841+Jebishajemi@users.noreply.github.com>
2025-09-15 16:14:01 +0530

    Time: 3 ms (79.64%), Space: 46.1 MB (33.82%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int[] candies = new int[n];
+        Arrays.fill(candies, 1); // everyone gets at least 1 candy
+        
+        // Left to right: give more candy if right child has higher rating
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+        
+        // Right to left: adjust for left neighbor
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+            }
+        }
+        
+        // Sum all candies
+        int sum = 0;
+        for (int c : candies) sum += c;
+        return sum;
+    }
+}
